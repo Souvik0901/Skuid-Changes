556e79c18cb7a0029f5119ac4ef0abf33c6bd71d
Souvik Sen <88578056+Souvik0901@users.noreply.github.com>
2024-12-31 12:37:52 +0530

    Create validateLoanAmount01.js

---
+++ validateLoanAmount01.js
@@ -0,0 +1,142 @@
+var params = arguments[0],
+$ = skuid.$;
+var app=skuid.model.getModel('FieldsForAmortization');
+var applicationData=app.getFirstRow();
+
+var displayMessage = function (message, severity) {
+    console.log(message);
+    console.log(severity);
+
+    var pageTitle = $('#summaryTab'); 
+    console.log(pageTitle);
+    var editor = pageTitle.data('object').editor;
+    console.log(editor);
+    
+    editor.messages.empty();
+    editor.handleMessages([
+        {
+            message: message,
+            severity: severity.toUpperCase()
+        }
+    ]);
+
+    return false;
+};
+
+///*********************************************** FOR LMAU -1720 ******************************************/
+if(applicationData.Insurance_Rebate_Amount__c !== null && applicationData.Insurance_Rebate_Amount__c !== undefined){
+   if((applicationData.Top_Up__c === true) && ((applicationData.Top_Up_Payout_Amount__c).toFixed(2) !== ((applicationData.CL_Contract__r.loan__Pay_Off_Amount_As_Of_Today__c - applicationData.Insurance_Rebate_Amount__c + 100).toFixed(2)))){
+    return displayMessage('Please Refresh Top Up Payout Amount prior changing Top Up Amount', 'ERROR');
+   } 
+}else if((applicationData.Top_Up__c === true) && ((applicationData.Top_Up_Payout_Amount__c).toFixed(2) !== ((applicationData.CL_Contract__r.loan__Pay_Off_Amount_As_Of_Today__c + 100).toFixed(2)))){
+    return displayMessage('Please Refresh Top Up Payout Amount prior changing Top Up Amount', 'ERROR');
+}
+
+///*********************************************** FOR LMAU -1938 ******************************************/
+if((applicationData.Top_Up__c === true )
+    && (applicationData.CL_Contract__r.loan__Pay_Off_Amount_As_Of_Today__c !== null) 
+    && ((applicationData.CL_Contract__r.loan__Pay_Off_Amount_As_Of_Today__c + applicationData.Top_Up_Amount__c) < 5000) ){
+        return displayMessage('The total loan amount (excluding the Loan Establishment Fee) must be at least $5,000. Please adjust the top-up amount if you wish to proceed.', 'ERROR');
+}
+
+
+///////**********************      For LMAU-3278 - Souvik Sen- SKUIDPAGE(Latitude_AmortizationSchedule)  *****************************/////////////////////
+
+// For New Application
+if (applicationData.Top_Up__c !== true && 
+    applicationData.Sales_Channel__c === 'Broker' && 
+    (applicationData.genesis__CL_Product_Name__c === 'Personal Loan Variable Rate' || 
+     applicationData.genesis__CL_Product_Name__c === 'Personal Loan Fixed Rate')) {
+
+    const loanAmount = applicationData.genesis__Loan_Amount__c;
+    const brokerFee = applicationData.broker_fee__c;
+
+    if (loanAmount >= 5000 && loanAmount <= 7499.99 && brokerFee > 200) {
+        return displayMessage('The Broker Fee cannot exceed $200 for loan amounts between $5,000-$7,499.99', 'ERROR');
+    }
+
+    if (loanAmount >= 7500 && loanAmount <= 9999.99 && brokerFee > 650) {
+        return displayMessage('The Broker Fee cannot exceed $650 for loan amounts between $7,500-$9,999.99', 'ERROR');
+    }
+
+    if (loanAmount >= 10000 && loanAmount <= 19999.99 && brokerFee > 990) {
+        return displayMessage('The Broker Fee cannot exceed $990 for loan amounts between $10,000-$19,999.99', 'ERROR');
+    }
+
+    if (loanAmount >= 20000 && loanAmount <= 49999.99 && brokerFee > 2200) {
+        return displayMessage('The Broker Fee cannot exceed $2,200 for loan amounts between $20,000-$49,999.99', 'ERROR');
+    }
+
+    if (loanAmount >= 50000 && brokerFee > 2500) {
+        return displayMessage('The Broker Fee cannot exceed $2,500 for loan amounts greater than or equal to $50,000', 'ERROR');
+    }
+}
+
+// For Motor Application
+else if (applicationData.Top_Up__c !== true && 
+    applicationData.Sales_Channel__c === 'Broker' && 
+    (applicationData.genesis__CL_Product_Name__c === 'Motor Loan Variable Rate' || 
+     applicationData.genesis__CL_Product_Name__c === 'Motor Loan Fixed Rate')) {
+
+    const loanAmount = applicationData.Total_Loan_Amount__c;
+    const brokerFee = applicationData.broker_fee__c;
+
+    if (loanAmount >= 5000 && loanAmount <= 7499.99 && brokerFee > 200) {
+        return displayMessage('The Broker Fee cannot exceed $200 for loan amounts between $5,000-$7,499.99', 'ERROR');
+    }
+
+    if (loanAmount >= 7500 && loanAmount <= 9999.99 && brokerFee > 650) {
+        return displayMessage('The Broker Fee cannot exceed $650 for loan amounts between $7,500-$9,999.99', 'ERROR');
+    }
+
+    if (loanAmount >= 10000 && loanAmount <= 19999.99 && brokerFee > 990) {
+        return displayMessage('The Broker Fee cannot exceed $990 for loan amounts between $10,000-$19,999.99', 'ERROR');
+    }
+
+    if (loanAmount >= 20000 && loanAmount <= 49999.99 && brokerFee > 2200) {
+        return displayMessage('The Broker Fee cannot exceed $2,200 for loan amounts between $20,000-$49,999.99', 'ERROR');
+    }
+
+    if (loanAmount >= 50000 && brokerFee > 2500) {
+        return displayMessage('The Broker Fee cannot exceed $2,500 for loan amounts greater than or equal to $50,000', 'ERROR');
+    }
+}
+
+
+
+
+
+
+//For Topup Application
+else if(applicationData.Top_Up__c === true && applicationData.Sales_Channel__c === 'Broker' ){
+    const loanAmount = applicationData.New_Cash__c;
+    const brokerFee = applicationData.broker_fee__c;
+
+    if (loanAmount <= 4999.99 && brokerFee > 0) {
+        return displayMessage('The Broker Fee cannot exceed $0 for loan amounts $4,999 or less', 'ERROR');
+    }
+
+    if (loanAmount >= 5000 && loanAmount <= 7499.99 && brokerFee > 200) {
+        return displayMessage('The Broker Fee cannot exceed $200 for loan amounts between $5,000-$7,499.99', 'ERROR');
+    }
+
+    if (loanAmount >= 7500 && loanAmount <= 9999.99 && brokerFee > 650) {
+        return displayMessage('The Broker Fee cannot exceed $650 for loan amounts between $7,500-$9,999.99', 'ERROR');
+    }
+
+    if (loanAmount >= 10000 && loanAmount <= 19999.99 && brokerFee > 990) {
+        return displayMessage('The Broker Fee cannot exceed $990 for loan amounts between $10,000-$19,999.99', 'ERROR');
+    }
+
+    if (loanAmount >= 20000 && loanAmount <= 49999.99 && brokerFee > 2200) {
+        return displayMessage('The Broker Fee cannot exceed $2,200 for loan amounts between $20,000-$49,999.99', 'ERROR');
+    }
+
+    if (loanAmount >= 50000 && brokerFee > 2500) {
+        return displayMessage('The Broker Fee cannot exceed $2,500 for loan amounts greater than or equal to $50,000', 'ERROR');
+    }
+}
+
+
+
+///////**********************  End Code Here  For LMAU-3278 - Souvik Sen- SKUIDPAGE(Latitude_AmortizationSchedule)  *****************************/////////////////////
